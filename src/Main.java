c90a0baeedc9a3aa67e5b644c9ba52fa3b7b1bb2
MIguel Esteban <esgmiguel04@gmail.com>
2024-10-08 12:58:54 +0200

    Started menu in main file and created operations class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido a la calculadora de la clase Main");
+
+        menu();
     }
 
-    public void menu(){
+    public static void menu(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("1. Sumar");
-        System.out.println("2. Multiplicar");
-        System.out.println("3. Restar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
         System.out.println("4. Dividir");
 
+        System.out.println("5. Salir");
+        System.out.println("Ingresa una opcion: ");
+
+        int opcion = 0;
+        try{
+            opcion=Integer.parseInt(scanner.nextLine());
+            switch(opcion){
+                case 1:
+                    System.out.println("Selecciono sumar: ");
+                    break;
+
+                    default:
+                        System.out.println("Opcion no valida");
+                        menu();
+                        break;
+            }
+        }catch(NumberFormatException e){
+            System.out.println("Ingresa un numero valido");
+            menu();
+        }
     }
 }
---
+++ src/Operations.java
@@ -0,0 +1,9 @@
+public class Operations {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
